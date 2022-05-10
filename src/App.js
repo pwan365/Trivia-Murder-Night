5c703cea0e850de03de0083ea1b0acf7fa7bbd65
myao209 <wayneyao99@gmail.com>
2022-05-10 19:53:44 +1200

    Updated homepage so it is fully rendered in 3D

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import "./App.scss";
 // components
 import HomeCanvas from "./components/HomeCanvas/HomeCanvas";
 import Header from "./components/Header/Header";
-import HomePara from "./components/HomePara/HomePara";
 import { BrowserRouter } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
@@ -13,11 +12,10 @@ const App = () => {
   return (
     <div id="root">
       <div style={{ height: "100%" }}>
-        <HomeCanvas />
         <BrowserRouter>
           <Header />
           <Routes>
-            <Route exact path="/" element={<HomePara />} />
+            <Route exact path="/" element={<HomeCanvas />} />
             {/* <Route path="/" element={} /> */}
           </Routes>
         </BrowserRouter>
