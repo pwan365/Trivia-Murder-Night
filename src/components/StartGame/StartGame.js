d8e83e95101738ec796e854ce0d052e7232a439f
ljey973 <ljey973@aucklanduni.ac.nz>
2022-05-15 02:12:42 +1200

    Updated modal pop up with styling.

--- src/components/StartGame/StartGame.js
+++ src/components/StartGame/StartGame.js
@@ -1,18 +1,23 @@
-import React from 'react'
+import { CategoryTwoTone } from '@mui/icons-material';
+import React, {useEffect,useState} from 'react'
 
-max_players = 8;
-max_rounds = 4;
 
-async function getCategories() {
-fetch('http://localhost:4200/api/services/categories')
-.then(response => response.json())
-.then(data => console.log(data));
-}
-
+const players = [2,3,4,5,6,7,8]
+var rounds = [1,2,3,4];
 
 export const StartGame = ({open,onClose}) => {
-    if (!open) return null;
+    const [category, setCategory] = useState([]);
+
+    useEffect(() => {
+        async function getCategories() {
+            const response = await fetch('http://localhost:4200/api/services/categories').then((resp) =>  resp.json());
+            setCategory(response);
+        }
     getCategories();
+    }, []);
+
+    if (!open) return null;
+    console.log(category);
     return (
         <>
          <div onClick={onClose} className='overlay'>
@@ -28,63 +33,50 @@ export const StartGame = ({open,onClose}) => {
              </p>
              <div className='content'>
                  <div className='createGame'>
+                     
                     <h3>Create Game</h3>
-                    <div class="listbox-area">
-                    <div>
-                        <span>
-                        Select a Category:
-                        </span>
-                        <ul>
-                        <li>
-                            Maths
-                        </li>
-                        <li>
-                            Astronomy
-                        </li>
-                        </ul>
-                    </div>
 
-                    <div>
-                        <span>
-                        Number of Players:
-                        </span>
-                        <ul>
-                        <li>
-                            Maths
-                        </li>
-                        <li>
-                            Astronomy
-                        </li>
-                        </ul>
-                    </div>
+                    <label>Select a Category:</label>
+                    <select name = "categories">
+                        {category.map(c => {
+                            <option>{c.name}</option>
+                            console.log(c.name);
+                        })}
+                    </select>
+                    <hr></hr>
 
-                    <div>
-                        <span>
-                        Number of Rounds:
-                        </span>
-                        <ul>
-                        <li>
-                            Maths
-                        </li>
-                        <li>
-                            Astronomy
-                        </li>
-                        </ul>
-                    </div>
+                    <label>Select Number of Players:</label>
+                    <br></br>
+                    <select>
+                        {players.map(player => (
+                            <option>{player}</option>
+                        ))}
+                    </select>
 
-                    </div>
+                    <hr></hr>
+
+                    <label>Choose Number of Rounds:</label>
+                    <br></br>
+                    <select>
+                    {rounds.map(round => (
+                            <option>{round}</option>
+                        ))}
+                    </select>
+
+                    <hr></hr>
+
                     <button>Start Game!</button>
-                 </div>
+                    </div>
                  <div className='joinGame'>
                     <h3>Join Game</h3>
                     <p>Enter Code:</p>
                     <input></input>
                     <button>Join!</button>
-                 </div>
+                    </div>
+                </div>
              </div>
            </div>
          </div>
-       </div>
         </>
     )
 }
