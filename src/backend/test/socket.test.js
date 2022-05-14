9aa2c561cb714e7d305dfa432e66c20d11833568
John Jia <chenhao0401@hotmail.com>
2022-05-15 03:46:46 +1200

    add tests

--- src/backend/test/socket.test.js
+++ src/backend/test/socket.test.js
@@ -39,45 +39,46 @@ describe("my awesome project", () => {
     });
 
     test("newRoom", (done) => {
-        serverSocket.on("hi", (cb) => {
-            cb("hola");
-        });
-        clientSocket.emit("hi", (arg) => {
-            expect(arg).toBe("hola");
+        const room = {};
+        serverSocket.on("newRoom", (arg) => {
+            room[arg] = {id: "lol"};
+            expect(arg).toBe("RoomCode");
+            serverSocket.emit("createRoom", room);
             done();
         });
+        clientSocket.emit("newRoom", "RoomCode");
+        clientSocket.on("createRoom", (arg) => {
+            expect(arg["RoomCode"].id).toBe("lol");
+            done();
+        })
     });
-
 
     test("newGame", (done) => {
-        serverSocket.on("hi", (cb) => {
-            cb("hola");
-        });
-        clientSocket.emit("hi", (arg) => {
-            expect(arg).toBe("hola");
+        serverSocket.on("joinRoom", (arg) => {
+            expect(arg).toBe("RoomCode");
+            serverSocket.emit("errorMessage", "Wrong CODE!");
             done();
         });
+        clientSocket.emit("joinRoom", "RoomCode");
+        clientSocket.on("errorMessage", (arg) => {
+            expect(arg).toBe("Wrong CODE!");
+            done();
+        })
     });
 
 
-    test("newRound", (done) => {
-        serverSocket.on("hi", (cb) => {
-            cb("hola");
-        });
-        clientSocket.emit("hi", (arg) => {
-            expect(arg).toBe("hola");
-            done();
-        });
-    });
 
     test("playerFinished", (done) => {
-        serverSocket.on("hi", (cb) => {
-            cb("hola");
-        });
-        clientSocket.emit("hi", (arg) => {
-            expect(arg).toBe("hola");
+        serverSocket.on("playerFinished", (arg) => {
+            expect(arg).toBe("RoomCode");
+            serverSocket.emit("Finished", "Finished");
             done();
         });
+        clientSocket.emit("playerFinished", "RoomCode");
+        clientSocket.on("Finished", (arg) => {
+            expect(arg).toBe("Finished");
+            done();
+        })
     });
 
 });
