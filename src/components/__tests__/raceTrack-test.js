ec8c7ff2238ec18d15dec7811a4dedec8d3bfc97
ljey973 <ljey973@aucklanduni.ac.nz>
2022-05-15 23:08:03 +1200

    Added test for race track

---
+++ src/components/__tests__/raceTrack-test.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {create} from 'react-test-renderer'
+import RacingTrack from '../RacingTrack/RacingTrack'
+
+var playerStatus =  {id: '4hTFDMJN4tz4So_MAAAL',
+                    playerName: 'player2',
+                    position: 0,
+                    roomCode: 'K8DYu'}
+
+const racingTrack = create (<RacingTrack 
+    playerStatus={playerStatus}
+    ghostPos={0}
+    numberOfRounds={5}/>);
+
+
+test('snapshot', () => {
+    expect(racingTrack).toMatchSnapshot();
+});
